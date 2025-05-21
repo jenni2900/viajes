8f8d947e54faa97cb6e7079d48d447c14250c86f
jenni2900 <jenni290024@outlook.com>
2025-05-20 19:43:29 -0500

    descripcion de cambios

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize ('database1','root','',{
-    host: '127.0.0.1',
-    port: '3306',
+const db = new Sequelize ('TablaViajes_somehowor','TablaViajes_somehowor','c3ca5b6dc225ef1437ebc493115568eff0dbcb41',{
+    host: '6mxiw.h.filess.io',
+    port: '3307',
     dialect: 'mysql',
     define:{
         timestamps: false
